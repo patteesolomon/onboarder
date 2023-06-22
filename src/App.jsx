77ec2e53dd15e30f4ae49f1f42e31603e082860a
CourageToGroww <Monkeysbuymainstream@gmail.com>
2023-06-21 23:13:36 -0500

    Finishing project up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
+import { useState } from 'react';
+import ContactList from './components/ContactList'
+import SelectedContact from './components/SelectedContact'
+
+
+/*Initialize a state variable, selectedContactId, with a default value of null.
+- Define a function, handleContactClick, that takes an ID and sets selectedContactId to that ID.
+- Conditionally render ContactList or SelectedContact based on whether selectedContactId is null or not.
+- Pass handleContactClick and selectedContactId to ContactList and SelectedContact via props.
+*/
+
+
 
 function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-      <div></div>
+      {selectedContactId ? (<SelectedContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId} />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   )
 }
-
 export default App
